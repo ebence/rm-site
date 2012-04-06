5cd239256b8b8c58f28f1a2c541f5acbcd45f338
Bence Erdos <bence.erdos@proinvento.hu>
2012-04-06 17:07:52 +0200

    Lets start!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RmSite::Application.config.secret_token = 'fecf44c95713f65533f97c161c99a49cd1a49d9b33f193ce1821f60fe1786183519637ea3d2800506f0ba7aea309907d076adc4a168c41f259ffedaa233955f8'
