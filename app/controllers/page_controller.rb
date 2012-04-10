6246ff8cd7f072a4b3f3341a01a84dcd97c81221
Attila Juhasz <juhasz@atti.la>
2012-04-10 19:55:44 +0200

    Remove Attila from Hungary visitors fun.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,11 +1,5 @@
 class PageController < ApplicationController
   def index
     @country = GeoIP.new('db/GeoIP.dat').country(request.remote_ip)
-  
-    if(@country.country_code == 77)
-    elsif @country.continent_code == "EU"
-    else
-    end
-    
   end
 end
